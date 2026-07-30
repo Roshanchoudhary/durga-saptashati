437efe2b3f1c58e3c18602113e0a038c4c263a12
Roshanchoudhary <roshanchoudhary@hotmail.com>
2026-07-30 11:11:37 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://durga-saptashati.pages.dev',
-  integrations: [mdx()],  // sitemap हटाया
+  integrations: [mdx()],
   output: 'static',
   build: {
     assets: '_astro'
